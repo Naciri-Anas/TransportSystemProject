f9425b0c2cd381ccb2f99fe2fe2e2ad7a6960d16
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2025-01-07 21:17:26 +0100

    Update main.c

--- main.c
+++ main.c
@@ -79,7 +79,7 @@ void ajouter_ligne(Reseau* reseau) {
 
     // Mise à jour du compteur de lignes
     reseau->nb_lignes++;
-    printf("Ligne ajouté avec succès !\n");
+    printf("Ligne ajoutée avec succès !\n");
 }
 
 // Fonction pour afficher les lignes
